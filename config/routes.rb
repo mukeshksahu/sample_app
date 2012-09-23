54abddf271dd9dc7926405c0684e99b37f88d845
Mukesh Kumar <mukeshksahu@gmail.com>
2012-09-23 17:27:40 +0530

    Finish User signup, Go to next chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
-
+  #get "users/new"
+  resources :users
   root :to => 'static_pages#home'
   match '/help' => 'static_pages#help'
   match '/about' => 'static_pages#about'
   match '/contact' => 'static_pages#contact'
 
   match '/signup' => 'users#new'
+
 
 
   # The priority is based upon order of creation:
